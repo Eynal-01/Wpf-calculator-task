9a29de9c7c6219f164202a8349d4d8bad777a9bb
Baxsh_cw07@itstep.edu.az <Baxsh_cw07@itstep.edu.az>
2023-02-25 16:28:36 +0400

    Strength missed

--- Wpf calculator task/MainWindow.xaml.cs
+++ Wpf calculator task/MainWindow.xaml.cs
@@ -18,11 +18,143 @@ namespace Wpf_calculator_task
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+    /// 
+
     public partial class MainWindow : Window
     {
+        private double valueFirst = 0.0;
+        private double valueSecond = 0.0;
+        private double result = 0.0;
+        private string operators = "";
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void ClearAll_Click(object sender, RoutedEventArgs e)
+        {
+            Resulttxtb.Clear();
+        }
+
+        private void ZeroBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (Resulttxtb.Text == "0")
+            {
+                Resulttxtb.Text = "0";
+            }
+            else
+            {
+                Resulttxtb.Text += "0";
+            }
+        }
+
+        private void Point_Click(object sender, RoutedEventArgs e)
+        {
+            if (!Resulttxtb.Text.Contains("."))
+            {
+                Resulttxtb.Text += ".";
+            }
+        }
+
+        private void OneBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var btn = (Button)sender;
+            if (Resulttxtb.Text == "0")
+            {
+                Resulttxtb.Text = btn.Content.ToString();
+            }
+            else
+            {
+                Resulttxtb.Text += btn.Content.ToString();
+            }
+        }
+
+        private void PlusMinus_Click(object sender, RoutedEventArgs e)
+        {
+            if (Resulttxtb.Text.Contains("-"))
+            {
+                Resulttxtb.Text = Resulttxtb.Text.Trim('-');
+            }
+            else
+            {
+                Resulttxtb.Text = "-" + Resulttxtb.Text;
+            }
+        }
+
+        private void Minus_Click(object sender, RoutedEventArgs e)
+        {
+            valueFirst = double.Parse(Resulttxtb.Text);
+            Resulttxtb.Clear();
+            operators = "-";
+        }
+        private void Plus_Click(object sender, RoutedEventArgs e)
+        {
+            valueFirst = double.Parse(Resulttxtb.Text);
+            Resulttxtb.Clear();
+            operators = "+";
+        }
+        private void Strength_Click(object sender, RoutedEventArgs e)
+        {
+            valueFirst = double.Parse(Resulttxtb.Text);
+            result=Math.Sqrt(valueFirst);
+            Resulttxtb.Text = result.ToString();
+        }
+
+        private void Multiplication_Click(object sender, RoutedEventArgs e)
+        {
+            valueFirst = double.Parse(Resulttxtb.Text);
+            Resulttxtb.Clear();
+            operators = "*";
+        }
+
+        private void Division_Click(object sender, RoutedEventArgs e)
+        {
+            valueFirst = double.Parse(Resulttxtb.Text);
+            Resulttxtb.Clear();
+            operators = "/";
+        }
+
+        private void Percent_Click(object sender, RoutedEventArgs e)
+        {
+            valueFirst = double.Parse(Resulttxtb.Text);
+            Resulttxtb.Clear();
+            operators = "%";
+        }
+        private void Equals_Click(object sender, RoutedEventArgs e)
+        {
+            switch (operators)
+            {
+                case "-":
+                    valueSecond = double.Parse(Resulttxtb.Text);
+                    result = valueFirst - valueSecond;
+                    Resulttxtb.Text = result.ToString();
+                    break;
+                case "+":
+                    valueSecond = double.Parse(Resulttxtb.Text);
+                    result = valueFirst + valueSecond;
+                    Resulttxtb.Text = result.ToString();
+                    break;
+                case "/":
+                    valueSecond = double.Parse(Resulttxtb.Text);
+                    result = valueFirst / valueSecond;
+                    Resulttxtb.Text = result.ToString();
+                    break;
+                case "*":
+                    valueSecond = double.Parse(Resulttxtb.Text);
+                    result = valueFirst * valueSecond;
+                    Resulttxtb.Text = result.ToString();
+                    break;
+                case "%":
+                    valueSecond = double.Parse(Resulttxtb.Text);
+                    result = (valueFirst * valueSecond) / 100;
+                    Resulttxtb.Text = result.ToString();
+                    break;
+            }
+        }
+
+        private void ClearOne_Click(object sender, RoutedEventArgs e)
+        {
+            Resulttxtb.Text = Resulttxtb.Text.Remove(Resulttxtb.Text.Length - 1);
         }
     }
 }
